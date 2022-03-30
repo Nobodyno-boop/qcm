190f1d29242d31974109d532cde95de3af16a736
Nobody <17411508+Nobodyno-boop@users.noreply.github.com>
2022-03-30 14:52:11 +0200

    add: add some work

--- src/Qcm/Qcm.php
+++ src/Qcm/Qcm.php
@@ -4,5 +4,22 @@ namespace App\Qcm;
 
 class Qcm
 {
+    /**
+     * @var array
+     */
+    private array $qcm;
+
+    /**
+     * @param $qcm
+     */
+    public function __construct($qcm)
+    {
+        $this->qcm = $qcm;
+    }
+
+    public function check(array $response): bool
+    {
+        return true;
+    }
 
 }
---
+++ src/Qcm/Question.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Qcm;
+
+use JetBrains\PhpStorm\ArrayShape;
+
+class Question
+{
+    private string $question;
+    private array $answers;
+    private int $correct;
+
+    /**
+     * @param string $question
+     * @param array $answers
+     * @param int $correct
+     */
+    public function __construct(string $question, array $answers, int $correct)
+    {
+        $this->question = $question;
+        $this->answers = $answers;
+        $this->correct = $correct;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuestion(): string
+    {
+        return $this->question;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAnswers(): array
+    {
+        return $this->answers;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCorrect(): int
+    {
+        return $this->correct;
+    }
+
+    #[ArrayShape(["question" => "string", "answers" => "array", "correct" => "int"])] public function toJson(): array
+    {
+        return [
+            "question" => $this->question,
+            "answers" => $this->answers,
+            "correct" => $this->correct
+        ];
+    }
+
+}
